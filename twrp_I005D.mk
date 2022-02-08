f8720857a3c8f089cd08f4305ebc4726e0f427c9
Captain Throwback <captainthrowback@hotmail.com>
2022-02-08 16:41:46 -0500

    I005D: updates for adsp loading support
    Also some tree cleanup
    
    Change-Id: I2bf846aa7ac369a68fb5e94a5c314f3e0e52cbc7

--- twrp_I005D.mk
+++ twrp_I005D.mk
@@ -43,6 +43,9 @@ $(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
 # OEM Info (automatically taken from device tree path)
 BOARD_VENDOR := $(or $(word 2,$(subst /, ,$(firstword $(MAKEFILE_LIST)))),$(value 2))
 
+# Common path for device trees
+COMMON_PATH := device/$(BOARD_VENDOR)/$(COMMON_SOC)-common
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_NAME := $(CUSTOM_VENDOR)_$(PRODUCT_DEVICE)
@@ -50,5 +53,8 @@ PRODUCT_BRAND := $(BOARD_VENDOR)
 PRODUCT_MODEL := $(shell echo $(PRODUCT_BRAND) | tr  '[:lower:]' '[:upper:]')_$(PRODUCT_DEVICE)
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
 
+# Default device path for tree
+DEVICE_PATH := device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)
+
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/device-$(PRODUCT_DEVICE).mk)
+$(call inherit-product, $(DEVICE_PATH)/device-$(PRODUCT_DEVICE).mk)
